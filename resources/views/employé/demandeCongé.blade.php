acd7892ee6adf25de5410e29da2c1a72dcb86b3e
SoniaAndrianantenaina <andrianantenainasonia@pulse.mg>
2023-09-25 16:58:12 +0300

    page demande de congé

--- resources/views/employé/demandeCongé.blade.php
+++ resources/views/employé/demandeCongé.blade.php
@@ -21,4 +21,39 @@
         </div>
     </section>
 
+    <section class="contenu">
+        <div class="div-grey">
+            @include('employé.profil.profil-infos')
+
+            <div class="div-grey__content">
+
+                <div class="title-h4 light-grey">
+                    <h4>Demander un congé</h4>
+                </div>
+
+                <div class="white-trait-lg"></div>
+
+
+                <div class="div-grey__request">
+                    <div class="div-grey__request__data">
+                        <p>Date début : </p>
+                        <input type="date">
+                    </div>
+
+                    <div class="div-grey__request__data">
+                        <p>Date fin : </p>
+                        <input type="date">
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="date-right">
+                <h6 class="title-h6 navy">Lundi 04 Septembre 2023</h6>
+            </div>
+
+        </div>
+    </section>
+
 </main>
