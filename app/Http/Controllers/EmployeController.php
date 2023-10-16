e8987d3eeb93c1e4b3ea5834b09df05b424dba79
SoniaAndrianantenaina <andrianantenainasonia@pulse.mg>
2023-10-16 16:55:02 +0300

    envoi mail et type contrat

--- app/Http/Controllers/EmployeController.php
+++ app/Http/Controllers/EmployeController.php
@@ -6,6 +6,7 @@ use App\Models\Employes;
 use App\Models\Candidats;
 use App\Models\TypeContrat;
 use App\Mail\TestMail;
+use App\Models\EmployesInfosPros;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
@@ -73,11 +74,15 @@ class EmployeController extends Controller
         if (auth()->check()) {
 
             $candidat = session()->get('candidat');
-
+            $idCandidat = $candidat->idCandidat;
+            $idDeptPoste = $candidat->idDeptPoste;
             $nom = $candidat->nom;
             $prenoms = $candidat->prenom;
             $email = $candidat->email;
+
             $type_contrat = $request->input('type_contrat');
+            $date_debut = $request->input('date_debut');
+            $date_fin = $request->input('date_fin');
 
             $firstsurname = $this->takeFirst($prenoms);
             $firstname = $this->takeFirst($nom);
@@ -86,10 +91,24 @@ class EmployeController extends Controller
             $identifiant = $firstsurname . '.' . $firstname;
             $mdp = $this->genererMdp(10);
             //echo $identifiant . $mdp;
+            //$mail = $this->envoyerMail($identifiant,$mdp,$email);
 
             $contrat = $this->genererContrat($type_contrat);
             echo $contrat;
-            //$mail = $this->envoyerMail($identifiant,$mdp,$email);
+
+            /*$idEmploye = Employes::insertGetId([
+                'idCandidat' => $idCandidat,
+                'identifiant' => $identifiant,
+                'mdp' => $mdp
+            ]);
+
+            $employeInfoPro = EmployesInfosPros::create([
+                'idEmploye' => $idEmploye,
+                'idDeptPoste' => $idDeptPoste,
+                'idTypeContrat' => $type_contrat,
+                'date_debut' => $date_debut,
+                'date_fin' => $date_fin
+            ]);*/
 
             return "E-mail envoyé avec succès";
         }
